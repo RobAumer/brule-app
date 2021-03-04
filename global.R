163517acae7737610fcbe09f26962c06b4efacb6
Rob Aumer <robaumer@Robs-MacBook-Pro.local>
2021-03-04 15:15:22 -0700

    finally gets the right pieces together for shinyapps.io deployment

--- global.R
+++
@@ -1,4 +0,0 @@
-library(reticulate)
-library(magrittr)
-
-reticulate::source_python("brule_wrapper.py", convert = FALSE)
--- server.R
+++ server.R
@@ -1,6 +1,23 @@
 library(shiny)
+library(reticulate)
+library(magrittr)
+
+# Define any Python packages needed for the app here:
+PYTHON_DEPENDENCIES = c('brule')
 
 shinyServer(function(input, output) {
+    
+    # ------------------ App virtualenv setup (Do not edit) ------------------- #
+    
+    virtualenv_dir = Sys.getenv('VIRTUALENV_NAME')
+    python_path = Sys.getenv('PYTHON_PATH')
+    
+    # Create virtual env and install dependencies
+    reticulate::virtualenv_create(envname = virtualenv_dir, python = python_path)
+    reticulate::virtualenv_install(virtualenv_dir, packages = PYTHON_DEPENDENCIES, ignore_installed=TRUE)
+    reticulate::use_virtualenv(virtualenv_dir, required = T)
+    
+    reticulate::source_python("brule_wrapper.py", convert = FALSE)
     
     brule_name <- eventReactive(input$check_it_out, {
         name <- brule_generator(input$first_name, input$last_name, sex()) %>%
