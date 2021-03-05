a054f447d77c6a89c09070034d1b9f5c79e71170
Rob Aumer <robaumer@Robs-MacBook-Pro.local>
2021-03-05 15:08:56 -0700

    Adds comments

--- server.R
+++ server.R
@@ -7,8 +7,7 @@ PYTHON_DEPENDENCIES = c('brule')
 
 shinyServer(function(input, output) {
     
-    # ------------------ App virtualenv setup (Do not edit) ------------------- #
-    
+    #App virtualenv setup. Works in conjunction with project .Rprofile
     virtualenv_dir = Sys.getenv('VIRTUALENV_NAME')
     python_path = Sys.getenv('PYTHON_PATH')
     
@@ -17,21 +16,27 @@ shinyServer(function(input, output) {
     reticulate::virtualenv_install(virtualenv_dir, packages = PYTHON_DEPENDENCIES, ignore_installed=TRUE)
     reticulate::use_virtualenv(virtualenv_dir, required = T)
     
+    # Source the wrapper code for the brule module
     reticulate::source_python("brule_wrapper.py", convert = FALSE)
     
+    # Generate a "brule name"
     brule_name <- eventReactive(input$check_it_out, {
         name <- brule_generator(input$first_name, input$last_name, sex()) %>%
             py_to_r()
         name
     })
     
+    # Convert the gender prompt 
     sex <- reactive({
         ifelse(test = input$sex == "Mrale", yes = "male", no = "female")
     })
+    
+    # Render the "brule name"
     output$brule_name <- renderText({
         paste("... ", brule_name())
     })
     
+    # Randomly select a Dr. Steven Brule Image
     image_data <- reactive({
         input$check_it_out # Force it to update each time
         image_src <- paste0("images/", sample(dir("images/"), size = 1))
@@ -41,6 +46,7 @@ shinyServer(function(input, output) {
              alt = " ")
     })
     
+    # Render the Dr. Steven Brule Image
     output$face <- renderImage(deleteFile = FALSE, {
         image_data()
     })
